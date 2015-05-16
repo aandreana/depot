56e66231523da2ea4b6d049d5148a7126d806399
Tony Andreana <tony.andreana@rogers.com>
2015-05-16 00:08:40 +0000

    Updated and repaired DB

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -6,8 +6,11 @@
 # We make no guarantees that this code is fit for any purpose. 
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
-class StoreController < ApplicationController
-  def index
-    @products = Product.order(:title)
+class LineItem < ActiveRecord::Base
+  belongs_to :product
+  belongs_to :cart
+
+  def total_price
+    product.price * quantity
   end
 end
